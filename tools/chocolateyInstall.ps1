941a63306fca719c335c2ed6cdcb152fa04ec882
Bob Tanner <tanner@real-time.com>
2021-09-11 22:40:06 -0500

    New release v6.1.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,21 +1,21 @@
 ﻿$packageName = 'scilab'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0.exe'
-$checksum = '0fe2a029cbd3bad9aa185534169c8a9e7c968636dc22e8dd20360ffc3067f0ad'
+$url = 'https://www.scilab.org/download/6.1.1/scilab-6.1.1.exe'
+$checksum = '43336cda96251d3dde9106fc5f1302ae722ab83e8a6bf894dff3238ea02be779'
 $checksumType = 'sha256'
-$url64 = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0_x64.exe'
-$checksum64 = 'c8a1c1db19aa832d2d7797bfd94f73f1e75aed5e07574555f815a552765cc279'
+$url64 = 'https://www.scilab.org/download/6.1.1/scilab-6.1.1_x64.exe'
+$checksum64 = '349f40c2b2906003ce82c4a8a37fc4dee4cf85bb3eb783020cc7e8ae815a6a0d'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -Url64bit "$url64" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType" `
-                          -Checksum64 "$checksum64" `
-                          -ChecksumType64 "$checksumType64"
+    -FileType "$installerType" `
+    -SilentArgs "$silentArgs" `
+    -Url "$url" `
+    -Url64bit "$url64" `
+    -ValidExitCodes $validExitCodes `
+    -Checksum "$checksum" `
+    -ChecksumType "$checksumType" `
+    -Checksum64 "$checksum64" `
+    -ChecksumType64 "$checksumType64"
