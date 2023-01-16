e995123554edae1a841b9214a961336c4e2a5aab
Bob Tanner <tanner@real-time.com>
2023-01-16 15:37:32 -0600

    Updated url, url64, and sha256

--- tools/data.ps1
+++ tools/data.ps1
@@ -1,8 +1,8 @@
 @{
-    $url = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0.exe'
-    $checksum = '0fe2a029cbd3bad9aa185534169c8a9e7c968636dc22e8dd20360ffc3067f0ad'
+    $url = ' https://oos.eu-west-2.outscale.com/scilab-releases/6.1.1/scilab-6.1.1.exe'
+    $checksum = '43336cda96251d3dde9106fc5f1302ae722ab83e8a6bf894dff3238ea02be779'
     $checksumType = 'sha256'
-    $url64 = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0_x64.exe'
-    $checksum64 = 'c8a1c1db19aa832d2d7797bfd94f73f1e75aed5e07574555f815a552765cc279'
+    $url64 = ' https://oos.eu-west-2.outscale.com/scilab-releases/6.1.1/scilab-6.1.1x64.exe'
+    $checksum64 = '349f40c2b2906003ce82c4a8a37fc4dee4cf85bb3eb783020cc7e8ae815a6a0d'
     $checksumType64 = 'sha256'
 }
