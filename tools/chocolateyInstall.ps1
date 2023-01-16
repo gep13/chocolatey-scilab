2ea89550058555798c7796ba035c25ac3f8d8a1a
Bob Tanner <tanner@real-time.com>
2023-01-16 16:17:56 -0600

    Another attempt to fix ERROR: A null key is not allowed in a hash literal.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,20 +4,20 @@ $data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -Chil
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-    $packageName    = $env:ChocolateyPackageName
+    packageName     = $env:ChocolateyPackageName
     unzipLocation   = $toolsDir
     $fileType       = 'exe'
     $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
     softwareName    = 'scilab'
-    $validExitCodes = @(0)
+    validExitCodes  = @(0)
 
-    url            = $data.url
-    checksum       = $data.checksum
-    checksumType   = $data.checksumType
+    url             = $data.url
+    checksum        = $data.checksum
+    checksumType    = $data.checksumType
 
     url64           = $data.url64
     checksum64      = $data.checksum64
     checksumType64  = $data.checksumType64
 }
 
-Install-ChocolateyPackage  @packageArgs
+Install-ChocolateyPackage @packageArgs
