3af00a558c19f033147be964f4bb70c351e6a719
Bob Tanner <tanner@real-time.com>
2023-01-16 16:32:02 -0600

    Fixed typo in 64-bit installer

--- tools/data.ps1
+++ tools/data.ps1
@@ -2,7 +2,7 @@
     url = 'https://oos.eu-west-2.outscale.com/scilab-releases/6.1.1/scilab-6.1.1.exe'
     checksum = '43336cda96251d3dde9106fc5f1302ae722ab83e8a6bf894dff3238ea02be779'
     checksumType = 'sha256'
-    url64 = 'https://oos.eu-west-2.outscale.com/scilab-releases/6.1.1/scilab-6.1.1x64.exe'
+    url64 = 'https://oos.eu-west-2.outscale.com/scilab-releases/6.1.1/scilab-6.1.1_x64.exe'
     checksum64 = '349f40c2b2906003ce82c4a8a37fc4dee4cf85bb3eb783020cc7e8ae815a6a0d'
     checksumType64 = 'sha256'
 }
