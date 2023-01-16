b48604e64297f621a6781ab1d1df45488e0e1313
Bob Tanner <tanner@real-time.com>
2023-01-16 15:16:28 -0600

    Closes #1: The remote file either doesn't exist, is unauthorized, or is forbidden for url #1
      * Refactor to put url and checksum into data.ps1
      * Added CI/CD

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,21 +1,23 @@
-﻿$packageName = 'scilab'
-$installerType = 'exe'
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-$url = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0.exe'
-$checksum = '0fe2a029cbd3bad9aa185534169c8a9e7c968636dc22e8dd20360ffc3067f0ad'
-$checksumType = 'sha256'
-$url64 = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0_x64.exe'
-$checksum64 = 'c8a1c1db19aa832d2d7797bfd94f73f1e75aed5e07574555f815a552765cc279'
-$checksumType64 = 'sha256'
-$validExitCodes = @(0)
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -SilentArgs "$silentArgs" `
-                          -Url "$url" `
-                          -Url64bit "$url64" `
-                          -ValidExitCodes $validExitCodes `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType" `
-                          -Checksum64 "$checksum64" `
-                          -ChecksumType64 "$checksumType64"
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+    $packageName    = $env:ChocolateyPackageName
+    unzipLocation   = $toolsDir
+    $fileType       = 'exe'
+    $silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+    softwareName    = 'scilab'
+    $validExitCodes = @(0)
+
+    url            = $data.url
+    checksum       = $data.checksum
+    checksumType   = $data.checksumType
+
+    url64           = $data.url64
+    checksum64      = $data.checksum64
+    checksumType64  = $data.checksumType64
+}
+
+Install-ChocolateyPackage  @packageArgs
---
+++ tools/data.ps1
@@ -0,0 +1,8 @@
+@{
+    $url = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0.exe'
+    $checksum = '0fe2a029cbd3bad9aa185534169c8a9e7c968636dc22e8dd20360ffc3067f0ad'
+    $checksumType = 'sha256'
+    $url64 = 'https://www.scilab.org/download/6.1.0/scilab-6.1.0_x64.exe'
+    $checksum64 = 'c8a1c1db19aa832d2d7797bfd94f73f1e75aed5e07574555f815a552765cc279'
+    $checksumType64 = 'sha256'
+}
