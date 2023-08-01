0821891ccb8a5d78c984460930d4f89b06dba8a7
Bob Tanner <tanner@real-time.com>
2023-08-01 18:01:51 -0500

    New release v2023.1.0

--- tools/data.ps1
+++ tools/data.ps1
@@ -1,5 +1,5 @@
 @{
-    url64 = 'https://www.scilab.org/download/2023.0.0/scilab-2023.0.0.bin.x64.exe'
-    checksum64 = '91fe57f4a9dc441fb8f818a2b4a4023dea579395a52332a3e7878ee82040e737'
+    url64 = 'https://www.scilab.org/download/2023.1.0/scilab-2023.1.0.bin.x64.exe'
+    checksum64 = '0e1b2ac60dcf0b9dcce52fa33b9511432528fcbd543bdf13a057723288bb19a4'
     checksumType64 = 'sha256'
 }
