2c071ac740fec390b44f20810ca7bd1145594dd0
Bob Tanner <tanner@real-time.com>
2024-07-05 09:34:42 -0500

    New release v2024.1.0

--- tools/data.ps1
+++ tools/data.ps1
@@ -1,5 +1,5 @@
 @{
-    url64 = 'https://www.scilab.org/download/2024.0.0/scilab-2024.0.0.bin.x64.exe'
-    checksum64 = 'c55548b552495bbe03878bd690e7d2a48f4049464af05f62eb092a6ffb5ecc8b'
+    url64 = 'https://www.scilab.org/download/2024.1.0/scilab-2024.1.0.bin.x64.exe'
+    checksum64 = '89217e95f2108d7c57933d32265a855212a4d9c97bb6953871fbe2a61719730a'
     checksumType64 = 'sha256'
 }
